7eab775f3c3c4e1b3bf7b4a787810d29ab709530
shonaloujacks <98349286+shonaloujacks@users.noreply.github.com>
2024-10-17 15:32:57 +0100

    Search and verify new url test added to home.spec.ts

--- tests/home.spec.ts
+++ tests/home.spec.ts
@@ -1,4 +1,5 @@
 import { test, expect } from "@playwright/test";
+import { url } from "inspector";
 
 test.describe("Homepage", () => {
   test("Open page and verify url", async ({ page }) => {
@@ -19,5 +20,25 @@ test.describe("Homepage", () => {
       .getByRole("link", { name: "womens-t20-wc-2024-logo" })
       .first();
     await expect(logo).toBeVisible();
+  });
+
+  test("Search and verify new url", async ({ page }) => {
+    // Go to homepage
+    await page.goto("https://www.icc-cricket.com/");
+
+    // Click on search icon
+    await page.getByLabel("Search").click();
+
+    //Select search field and fill
+
+    const searchInput = page.getByPlaceholder("what are you looking for?");
+
+    await searchInput.fill("India");
+
+    // Click search
+    await searchInput.press("Enter");
+
+    // Assert URL to contain search?q=india
+    expect(page.url()).toContain("search?q=India");
   });
 });
