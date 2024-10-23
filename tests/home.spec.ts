d7d923f966e173eb933c726791d56445ac867652
shonaloujacks <98349286+shonaloujacks@users.noreply.github.com>
2024-10-23 15:11:48 +0100

    Added test to verify menu tabs text and links

--- tests/home.spec.ts
+++ tests/home.spec.ts
@@ -31,7 +31,7 @@ test.describe("Homepage", () => {
     );
 
     // Click on search icon
-    await page.getByLabel("Search").click();
+    await page.getByLabel("Search").first().click();
 
     //Select search field and fill
 
@@ -44,5 +44,40 @@ test.describe("Homepage", () => {
 
     // Assert URL to contain search?q=india
     expect(page.url()).toContain("search?q=India");
+  });
+
+  test.only("Verify menu tabs text and link", async ({ page }) => {
+    // Go to homepage
+    console.log("************** TEST ******************");
+
+    await page.goto(
+      "https://www.icc-cricket.com/tournaments/womens-t20-worldcup"
+    );
+
+    // Check that the text matches the menu tabs
+    const expectedMenuText = [
+      "icc home",
+      "matches",
+      "standings",
+      "stats",
+      "teams",
+      "news",
+      "videos",
+      "predictor",
+      "ticketing",
+      "more",
+    ];
+
+    const actualMenuText = await page
+      .locator("[id^='primaryNavigation_'] .menu-view .menu-list-wrapper > div")
+      .allInnerTexts();
+
+    const normalizeArray = (arr) =>
+      arr.map((item) => item.trim().toLowerCase());
+
+    const normalizedActualMenuText = normalizeArray(actualMenuText);
+    const normalizedExpectedMenuText = normalizeArray(expectedMenuText);
+
+    expect(await normalizedActualMenuText).toEqual(normalizedExpectedMenuText);
   });
 });
